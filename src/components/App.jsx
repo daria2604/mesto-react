d2ff44cfe41464be6f0e42350934cb65852590ea
Daria Rutts <darya.rutts@yandex.ru>
2023-05-19 14:06:18 +0300

    refactor: вынести компонент попапов

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
+import PopupWithForm from "./PopupWithForm";
 
 function App() {
   return (
@@ -10,18 +11,12 @@ function App() {
           <Header/>
           <Main/>
           <Footer/>
-        <div className="popup popup_type_edit">
-          <div className="popup__container">
-            <button className="button button_action_close" type="button" />
-            <form
-              className="popup__form popup__form_type_edit"
-              name="editForm"
-              autoComplete="off"
-            >
-              <h3 className="popup__heading popup__heading_type_edit">
-                Редактировать профиль
-              </h3>
-              <input
+          <PopupWithForm
+            name="edit"
+            title="Редактировать профиль"
+            button="Сохранить"
+          >
+            <input
                 type="text"
                 placeholder="Имя"
                 className="popup__input popup__input_type_name"
@@ -43,25 +38,28 @@ function App() {
                 required=""
               />
               <span className="popup__input-error popup__input-error_type_about" />
-              <button
-                type="submit"
-                className="popup__submit-button popup__submit-button_type_edit"
-              >
-                Сохранить
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="popup popup_type_add">
-          <div className="popup__container">
-            <button className="button button_action_close" type="button" />
-            <form
-              className="popup__form popup__form_type_add"
-              name="addForm"
-              autoComplete="off"
-            >
-              <h3 className="popup__heading popup__heading_type_add">Новое место</h3>
-              <input
+          </PopupWithForm>
+          <PopupWithForm
+            name="avatar"
+            title="Обновить аватар"
+            button="Сохранить"
+          >
+            <input
+                type="url"
+                placeholder="Ссылка на картинку"
+                className="popup__input popup__input_type_avatar"
+                name="avatar-link"
+                defaultValue=""
+                required=""
+              />
+              <span className="popup__input-error popup__input-error_type_avatar-link" />
+          </PopupWithForm>
+          <PopupWithForm
+            name="add"
+            title="Новое место"
+            button="Создать"
+          >
+            <input
                 type="text"
                 placeholder="Название"
                 className="popup__input popup__input_type_title"
@@ -81,16 +79,14 @@ function App() {
                 required=""
               />
               <span className="popup__input-error popup__input-error_type_link" />
-              <button
-                type="submit"
-                className="popup__submit-button popup__submit-button_type_add"
-              >
-                Создать
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="popup popup_type_image">
+          </PopupWithForm>
+          <PopupWithForm
+            name="confirm"
+            title="Вы уверены?"
+            button="Да"
+          ></PopupWithForm>
+
+        {/* <div className="popup popup_type_image">
           <div className="popup__container">
             <button className="button button_action_close" type="button" />
             <div className="popup__wrap">
@@ -98,52 +94,7 @@ function App() {
               <p className="popup__caption" />
             </div>
           </div>
-        </div>
-        <div className="popup popup_type_confirm">
-          <div className="popup__container">
-            <button className="button button_action_close" type="button" />
-            <form className="popup__form popup__form_type_confirm" name="confirmForm">
-              <h3 className="popup__heading popup__heading_type_confirm">
-                Вы уверены?
-              </h3>
-              <button
-                type="submit"
-                className="popup__submit-button popup__submit-button_type_confirm"
-              >
-                Да
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="popup popup_type_avatar">
-          <div className="popup__container">
-            <button className="button button_action_close" type="button" />
-            <form
-              className="popup__form popup__form_type_avatar"
-              name="avatarForm"
-              autoComplete="off"
-            >
-              <h3 className="popup__heading popup__heading_type_avatar">
-                Обновить аватар
-              </h3>
-              <input
-                type="url"
-                placeholder="Ссылка на картинку"
-                className="popup__input popup__input_type_avatar"
-                name="avatar-link"
-                defaultValue=""
-                required=""
-              />
-              <span className="popup__input-error popup__input-error_type_avatar-link" />
-              <button
-                type="submit"
-                className="popup__submit-button popup__submit-button_type_avatar"
-              >
-                Сохранить
-              </button>
-            </form>
-          </div>
-        </div>
+        </div> */}
         </div>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,24 @@
 import React from "react"
 import avatar from "../images/avatar.jpg"
 
-const Main = () => {
+function Main() {
+  function handleEditProfileClick() {
+    document.querySelector('.popup_type_edit').classList.add('popup_opened')
+  }
+
+  function handleAddPlaceClick() {
+    document.querySelector('.popup_type_add').classList.add('popup_opened')
+  }
+
+  function handleEditAvatarClick() {
+    document.querySelector('.popup_type_avatar').classList.add('popup_opened')
+  }
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__avatar-container">
-          <button className="button button_action_edit-avatar" type="button" />
+          <button className="button button_action_edit-avatar" type="button" onClick={handleEditAvatarClick}/>
           <img
             src={avatar}
             alt="Аватар"
@@ -16,11 +28,11 @@ const Main = () => {
         <div className="profile__info">
           <div className="profile__title">
             <h1 className="profile__name">Жак-Ив Кусто</h1>
-            <button className="button button_action_edit" type="button" />
+            <button className="button button_action_edit" type="button" onClick={handleEditProfileClick}/>
           </div>
           <p className="profile__about">Исследователь океана</p>
         </div>
-        <button className="button button_action_add" type="button" />
+        <button className="button button_action_add" type="button" onClick={handleAddPlaceClick}/>
       </section>
       <section className="gallery">
         <div className="cards">
