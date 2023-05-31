9f65c6b0904712cb3e79abfac50e1e04313c62b2
Daria Rutts <darya.rutts@yandex.ru>
2023-05-31 14:34:13 +0300

    refactor: подписать на контекст Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,21 @@
 import React from "react"
+import { CurrentUserContext } from "../contexts/CurrentUserContext"
 
 function Card({ card, onCardClick }) {
+  const currentUser = React.useContext(CurrentUserContext)
+  const isOwn = card.owner._id === currentUser._id
+  const isLiked = card.likes.some(i => i._id === currentUser._id)
+  const cardLikeButtonClassName = (
+    `button__like ${isLiked && 'button__like_active'}`
+  )
+
   function handleClick() {
     onCardClick(card)
   }
 
   return (
     <div className="card">
-    <button className="button button_action_delete" type="button" />
+    {isOwn && <button className="button button_action_delete" type="button" />}
     <img
       className="card__image"
       alt={card.name}
@@ -17,7 +25,7 @@ function Card({ card, onCardClick }) {
     <div className="card__info">
       <h2 className="card__title">{card.name}</h2>
       <div className="card__like">
-        <button className="button button__like" type="button" />
+        <button className={`button ${cardLikeButtonClassName}`} type="button" />
         <p className="card__like_counter">{card.likes.length}</p>
       </div>
       </div>
